0691a84cc539cc18a3fe93f4a084438d73f73783
Nilo18 <nikalongurashvili@gmail.com>
2025-02-02 15:46:10 +0400

    Remove default tests and add money tests

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,5 @@
 export function formatCurrency(priceCents) {
-    return (Math.round(priceCents / 100)).toFixed(2); // Use Math.round() since toFixed() doesn't round numbers correctly sometimes
+    return (Math.round(priceCents) / 100).toFixed(2); // Use Math.round() since toFixed() doesn't round numbers correctly sometimes
 }
+
+export default formatCurrency
